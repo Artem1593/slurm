1722ad80782e91465c00cb0ab37658bab6eef412
Nancy Kritkausky <nancy.kritkausky@atos.net>
2016-11-01 13:55:12 -0700

    Make some corrections after rebase

--- src/common/slurm_protocol_defs.c
+++ src/common/slurm_protocol_defs.c
@@ -781,9 +781,6 @@ extern void slurm_free_job_desc_msg(job_desc_msg_t * msg)
 		for (i = 0; i < msg->spank_job_env_size; i++)
 			xfree(msg->spank_job_env[i]);
 		xfree(msg->spank_job_env);
-		for (i = 0; i < msg->pelog_env_size; i++)
-			xfree(msg->pelog_env[i]);
-		xfree(msg->pelog_env);
 		xfree(msg->tres_req_cnt);
 		xfree(msg->wckey);
 		xfree(msg->work_dir);
@@ -827,9 +824,6 @@ extern void slurm_free_prolog_launch_msg(prolog_launch_msg_t * msg)
 		for (i = 0; i < msg->spank_job_env_size; i++)
 			xfree(msg->spank_job_env[i]);
 		xfree(msg->spank_job_env);
-		for (i = 0; i < msg->pelog_env_size; i++)
-			xfree(msg->pelog_env[i]);
-		xfree(msg->pelog_env);
 		slurm_cred_destroy(msg->cred);
 
 		xfree(msg);
@@ -881,12 +875,6 @@ extern void slurm_free_job_launch_msg(batch_job_launch_msg_t * msg)
 			for (i = 0; i < msg->spank_job_env_size; i++)
 				xfree(msg->spank_job_env[i]);
 			xfree(msg->spank_job_env);
-		}
-
-		if (msg->pelog_env) {
-			for (i = 0; i < msg->pelog_env_size; i++)
-				xfree(msg->pelog_env[i]);
-			xfree(msg->pelog_env);
 		}
 
 		if (msg->environment) {
@@ -1156,9 +1144,6 @@ extern void slurm_free_kill_job_msg(kill_job_msg_t * msg)
 		for (i=0; i<msg->spank_job_env_size; i++)
 			xfree(msg->spank_job_env[i]);
 		xfree(msg->spank_job_env);
-		for (i=0; i<msg->pelog_env_size; i++)
-			xfree(msg->pelog_env[i]);
-		xfree(msg->pelog_env);
 		xfree(msg);
 	}
 }
@@ -1215,10 +1200,6 @@ extern void slurm_free_launch_tasks_request_msg(launch_tasks_request_msg_t * msg
 		xfree(msg->spank_job_env[i]);
 	}
 	xfree(msg->spank_job_env);
-	for (i = 0; i < msg->pelog_env_size; i++) {
-		xfree(msg->pelog_env[i]);
-	}
-	xfree(msg->pelog_env);
 	if (msg->nnodes && msg->global_task_ids)
 		for(i=0; i<msg->nnodes; i++) {
 			xfree(msg->global_task_ids[i]);
